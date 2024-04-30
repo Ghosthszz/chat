e597d4710f354e58c0200ea95be31579881e03a7
Gustavo <142137414+Ghosthszz@users.noreply.github.com>
2024-04-30 13:08:44 -0300

    Create scripts.js

---
+++ frontend/js/scripts.js
@@ -0,0 +1,133 @@
+// Elementos de login
+const login = document.querySelector(".login");
+const loginForm = login.querySelector(".login__form");
+const loginInput = login.querySelector(".login__input");
+
+// Elementos do chat
+const chat = document.querySelector(".chat");
+const chatForm = chat.querySelector(".chat__form");
+const chatInput = chat.querySelector(".chat__input")
+const chatMessages = chat.querySelector(".chat__messages");
+
+const colors = [
+    "cadetblue",
+    "darkgoldenrod",
+    "cornflowerblue",
+    "darkkhaki",
+    "hotpink",
+    "gold"
+];
+
+const user = { id: "", name: "", color: "" };
+
+let websocket;
+
+const createMessageSelfElement = (content) => {
+    const div = document.createElement("div");
+
+    div.classList.add("message--self");
+    div.innerHTML = content;
+
+    return div;
+};
+
+const createMessageOtherElement = (content, sender, senderColor) => {
+    const div = document.createElement("div");
+    const span = document.createElement("span");
+
+    div.classList.add("message--other");
+
+    span.classList.add("message--sender");
+    span.style.color = senderColor;
+
+    div.appendChild(span);
+
+    span.innerHTML = sender;
+    div.innerHTML += content;
+
+    return div;
+};
+
+const getRandomColor = () => {
+    const randomIndex = Math.floor(Math.random() * colors.length);
+    return colors[randomIndex];
+};
+
+const scrollScreen = () => {
+    window.scrollTo({
+        top: document.body.scrollHeight,
+        behavior: "smooth"
+    });
+};
+
+const playNotificationSound = () => {
+    const audio = new Audio("./sounds/aviso.mp3");
+    audio.play();
+};
+
+const processMessage = ({ data }) => {
+    const { userId, userName, userColor, content } = JSON.parse(data);
+
+    const isCurrentUser = userId === user.id;
+
+    const message = createMessageOtherElement(content, userName, userColor);
+
+    chatMessages.appendChild(message);
+
+    scrollScreen();
+
+    if (!isCurrentUser) {
+        console.log("Nova mensagem recebida:", content);
+        playNotificationSound();
+    }
+};
+
+const handleLogin = (event) => {
+    event.preventDefault();
+
+    user.id = crypto.randomUUID();
+    user.name = loginInput.value;
+    user.color = getRandomColor();
+
+    login.style.display = "none";
+    chat.style.display = "flex";
+
+    websocket = new WebSocket("ws://localhost:8080");
+    websocket.onmessage = processMessage;
+    websocket.onopen = () => {
+        console.log("Conexão WebSocket estabelecida com sucesso.");
+
+ const entryMessage = {
+            userId: user.id,
+            userName: `<div style="display: inline-block; margin-right: 30px; border-radius: 90%;">
+            <img src="images/sistema.png" alt="Teste" style="vertical-align: middle; width: 30px; height: 30px; margin-right: 10px;">
+            <h1 style="display: inline-block; vertical-align: middle; font-size: 15px; margin: 0;">Sistema</h1>
+        </div>
+        `,
+            userColor: "#7D5AC1",
+            content: `${user.name} entrou no chat!`
+        };
+        
+
+        console.log("Enviando mensagem de entrada:", entryMessage);
+        websocket.send(JSON.stringify(entryMessage));
+    };
+};
+
+const sendMessage = (event) => {
+    event.preventDefault();
+
+    const message = {
+        userId: user.id,
+        userName: user.name,
+        userColor: user.color,
+        content: chatInput.value
+    };
+
+    websocket.send(JSON.stringify(message));
+
+    chatInput.value = "";
+};
+
+loginForm.addEventListener("submit", handleLogin);
+chatForm.addEventListener("submit", sendMessage);
