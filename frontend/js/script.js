5f0fe3e02bdbfbfd039c349f723fe78fe2a443df
Gustavo <142137414+Ghosthszz@users.noreply.github.com>
2024-08-13 10:42:14 -0300

    Update script.js

--- frontend/js/script.js
+++ frontend/js/script.js
@@ -161,7 +161,7 @@ const handleLogin = (event) => {
     login.style.display = "none";
     chat.style.display = "flex";
 
-    websocket = new WebSocket("ws://localhost:8080");
+    websocket = new WebSocket("wss://chat-ghosthszz.onrender.com");
     websocket.onmessage = processMessage;
     websocket.onopen = () => {
         console.log("Conexão WebSocket estabelecida com sucesso.");
