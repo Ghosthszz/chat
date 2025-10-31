39a035b5180fe59d6f55375f5fbccb9c6d8b7695
Gustavo <142137414+Ghosthszz@users.noreply.github.com>
2025-10-31 12:54:57 +0000

    UPDATE ROUTES

--- backend/src/server.js
+++ backend/src/server.js
@@ -9,7 +9,7 @@ const app = express();
 const port = process.env.PORT || 8080;
 
 app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "public", "chat"));
+  res.sendFile(path.join(__dirname, "public", "odra.html"));
 });
 
 app.use(express.static(path.join(__dirname, "public")));
